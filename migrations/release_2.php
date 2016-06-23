8b3925415fe8b8f9c6605d03d711f8423e4c0d7d
pierrdu <pierre@duhem.com>
2016-06-23 11:41:36 +0200

    1.0.7b

--- migrations/release_2.php
+++ migrations/release_2.php
@@ -11,16 +11,24 @@ namespace lmdi\trashbin\migrations;
 
 class release_2 extends \phpbb\db\migration\migration
 {
-	protected $trashbin;
-
 	public function effectively_installed()
 	{
-		return isset($this->config['lmdi_trashbin']);
+		return isset($this->config['lmdi_trashbin2']);
 	}
+
 
 	static public function depends_on()
 	{
 		return array('\lmdi\trashbin\migrations\release_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Configuration rows
+			array('config.add', array('lmdi_trashbin2', 0)),
+
+		);
 	}
 
 
@@ -28,19 +36,19 @@ class release_2 extends \phpbb\db\migration\migration
 	{
 		return array(
 			array('custom', array(array(&$this, 'reset_pruning_state'))),
+			array('config.remove', array('lmdi_trashbin2')),
 		);
 	}
 
 
 	public function reset_pruning_state()
 	{
-		global $config;
+		global $config, $db;
 		$trashbin = (int) $config['lmdi_trashbin'];
 		$sql = 'UPDATE '. FORUMS_TABLE . '
 			SET enable_prune=0 
 			WHERE forum_id='.$trashbin;
-		$this->db->sql_query($sql);
+		$db->sql_query($sql);
 	}
-
 
 }
