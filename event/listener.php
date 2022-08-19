332d9c76cbc7ea72be223ba1297ea8d331f0edba
Mario Skouat <forum@skouat.org>
2022-08-19 21:51:27 +0200

    Code improvement and fix (white)spacing, indentation, etc.

--- event/listener.php
+++ event/listener.php
@@ -1,22 +1,23 @@
 <?php
 /**
-*
-* @package phpBB Extension - LMDI Trashbin
-* @copyright (c) 2016-2021 LMDI - Pierre Duhem
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package phpBB Extension - LMDI Trashbin
+ * @copyright (c) 2016-2021 LMDI - Pierre Duhem
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 namespace lmdi\trashbin\event;
 
 /**
-* @ignore
-*/
+ * @ignore
+ */
+
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
-* Event listener
-*/
+ * Event listener
+ */
 class listener implements EventSubscriberInterface
 {
 	protected $user;
@@ -29,8 +30,8 @@ class listener implements EventSubscriberInterface
 	protected $request;
 	protected $auth;
 	protected $phpbb_log;
-	protected $fid;	// Class global because of function build_url
-	protected $tid;
+	protected $fid;    // Global property because of function build_url
+	protected $tid;    // Global property because of function build_url
 
 	public function __construct(
 		\phpbb\db\driver\driver_interface $db,
@@ -43,7 +44,7 @@ class listener implements EventSubscriberInterface
 		\phpbb\log\log $log,
 		$root_path,
 		$phpEx
-		)
+	)
 	{
 		$this->db = $db;
 		$this->config = $config;
@@ -58,15 +59,14 @@ class listener implements EventSubscriberInterface
 	}
 
 
-	static public function getSubscribedEvents ()
+	static public function getSubscribedEvents()
 	{
-	return array(
-		'core.user_setup'				=> 'load_language_on_setup',
-		'core.page_header'				=> 'build_url',
-		'core.viewtopic_get_post_data'	=> 'move_topic',
+		return array(
+			'core.user_setup'              => 'load_language_on_setup',
+			'core.page_header'             => 'build_url',
+			'core.viewtopic_get_post_data' => 'move_topic',
 		);
 	}
-
 
 	public function load_language_on_setup($event)
 	{
@@ -74,124 +74,111 @@ class listener implements EventSubscriberInterface
 		$lang_set_ext[] = array(
 			'ext_name' => 'lmdi/trashbin',
 			'lang_set' => 'trashbin',
-			);
+		);
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
-
 	public function build_url($event)
 	{
-		$target = $this->config['lmdi_trashbin'];
-		// Moderator with right to move or delete, trashbin configured and we aren't in the trashbin
-		if (($this->auth->acl_get('m_delete', $this->fid) || $this->auth->acl_get('m_move', $this->fid)) && $target && ($target != $this->fid))
+		// Moderator with right to move or delete and we aren't in the trashbin
+		if ($this->can_build_url())
 		{
 			$params = "f=$this->fid&amp;t=$this->tid&amp;trash=1";
 			$url = append_sid($this->root_path . 'viewtopic.' . $this->phpEx, $params);
 			$this->template->assign_vars(array(
-				'U_TRASHBIN'	=> $url,
-				'S_TRASHBIN'	=> true,
-				));
-		}
-		else
-		{
-		$this->template->assign_vars(array(
-			'S_TRASHBIN'	=> false,
+				'U_TRASHBIN' => $url,
+				'S_TRASHBIN' => true,
 			));
 		}
 	}
 
+	private function can_build_url()
+	{
+		return (int) $this->config['lmdi_trashbin'] !== $this->fid && $this->template->retrieve_var('S_VIEWTOPIC') && ($this->auth->acl_get('m_delete') || $this->auth->acl_get('m_move'));
+	}
 
 	public function move_topic($event)
 	{
-		$this->fid = $event['forum_id'];
-		$this->tid = $event['topic_id'];
+		$this->fid = (int) $event['forum_id'];
+		$this->tid = (int) $event['topic_id'];
 		$trash = $this->request->variable('trash', 0);
 		if ($trash)
 		{
-			$user_id = $this->user->data['user_id'];
-			$user_name = $this->user->data['username'];
-			$target = $this->config['lmdi_trashbin'];
+			$target = (int) $this->config['lmdi_trashbin'];
 			// Trashbin already configured and we aren't within this forum
-			if ($target != 0 && $this->fid != $target)
+			// Various permissions
+			if ($target !== 0 && $this->fid !== $target && ($this->auth->acl_get('m_delete') || $this->auth->acl_get('m_move')) && ($this->auth->acl_get('f_noapprove', $target) && $this->auth->acl_get('f_list', $target)))
 			{
-				// Various permissions
-				if (($this->auth->acl_get('m_delete', $this->fid) || $this->auth->acl_get('m_move', $this->fid)) && ($this->auth->acl_get('f_noapprove', $target) && $this->auth->acl_get('f_list', $target)))
+				if (!function_exists('move_topics'))
 				{
-					if (!function_exists('move_topics'))
-					{
-						include($this->root_path . 'includes/functions_admin.' . $this->phpEx);
-					}
-					if (!function_exists('submit_post'))
-					{
-						include($this->root_path . 'includes/functions_posting.' . $this->phpEx);
-					}
+					include($this->root_path . 'includes/functions_admin.' . $this->phpEx);
+				}
+				if (!function_exists('submit_post'))
+				{
+					include($this->root_path . 'includes/functions_posting.' . $this->phpEx);
+				}
 
-					// Deletion of the subscriptions to the topic moved to trashbin
-					$sql = 'DELETE FROM ' . TOPICS_WATCH_TABLE . '
-						WHERE topic_id=' . (int) $this->tid;
-					$this->db->sql_query($sql);
+				// Deletion of the subscriptions to the topic moved to trashbin
+				$sql = 'DELETE FROM ' . TOPICS_WATCH_TABLE . '
+					WHERE topic_id=' . (int) $this->tid;
+				$this->db->sql_query($sql);
 
-					// Creation of a post (with same subject line and date = today)
-					// to keep the topic alive
-					$uid = $bitfield = $options = '';
-					$topic_data = $event['topic_data'];
-					$forum_name = $topic_data['forum_name'];
-					$subject = $topic_data['topic_title'];
-					generate_text_for_storage($subject, $uid, $bitfield, $options, false, false, false);
-					$subject = str_replace ('<t>', '', $subject);
-					$subject = str_replace ('</t>', '', $subject);
-					$post_text = $this->language->lang('TRASHBIN_TEXT', $user_name, $forum_name);
-					generate_text_for_storage($post_text, $uid, $bitfield, $options, true, true, true);
-					$data = array(
-						'forum_id'		=> $this->fid,
-						'topic_id'		=> $this->tid,
-						'icon_id'			=> false,
-						'enable_bbcode'	=> true,
-						'enable_smilies'	=> true,
-						'enable_urls'		=> true,
-						'enable_sig'		=> true,
-						'message'			=> $post_text,
-						'message_md5'		=> md5($post_text),
-						'bbcode_bitfield'	=> $bitfield,
-						'bbcode_uid'		=> $uid,
-						'post_edit_locked'	=> 0,
-						'topic_title'		=> $subject,
-						'notify_set'		=> false,
-						'notify'			=> false,
-						'post_time'		=> 0,
-						'forum_name'		=> '',
-						'enable_indexing'	=> true,
-						);
-					$poll = array();
-					submit_post('reply', $subject, '', POST_NORMAL, $poll, $data);
+				// Creation of a post (with same subject line and date = today)
+				// to keep the topic alive
+				$uid = $bitfield = $options = '';
+				$topic_data = $event['topic_data'];
+				$forum_name = $topic_data['forum_name'];
+				$subject = $topic_data['topic_title'];
+				generate_text_for_storage($subject, $uid, $bitfield, $options, false, false, false);
+				$subject = str_replace(array('<t>', '</t>'), '', $subject);
+				$post_text = $this->language->lang('TRASHBIN_TEXT', $this->user->data['username'], $forum_name);
+				generate_text_for_storage($post_text, $uid, $bitfield, $options, true, true, true);
+				$data = array(
+					'forum_id'         => $this->fid,
+					'topic_id'         => $this->tid,
+					'icon_id'          => false,
+					'enable_bbcode'    => true,
+					'enable_smilies'   => true,
+					'enable_urls'      => true,
+					'enable_sig'       => true,
+					'message'          => $post_text,
+					'message_md5'      => md5($post_text),
+					'bbcode_bitfield'  => $bitfield,
+					'bbcode_uid'       => $uid,
+					'post_edit_locked' => 0,
+					'topic_title'      => $subject,
+					'notify_set'       => false,
+					'notify'           => false,
+					'post_time'        => 0,
+					'forum_name'       => '',
+					'enable_indexing'  => true,
+				);
+				$poll = array();
+				submit_post('reply', $subject, '', POST_NORMAL, $poll, $data);
 
-					// Moving and resetting the topic_type to normal
-					move_topics(array($this->tid), $target);
-					$sql = 'UPDATE ' . TOPICS_TABLE . ' SET topic_type = 0, topic_status = 0
-						WHERE topic_id = ' . (int) $this->tid;
-					$this->db->sql_query($sql);
+				// Moving and resetting the topic_type to normal
+				move_topics(array($this->tid), $target);
+				$sql = 'UPDATE ' . TOPICS_TABLE . ' SET topic_type = 0, topic_status = 0
+					WHERE topic_id = ' . (int) $this->tid;
+				$this->db->sql_query($sql);
 
-					// Logging
-					$trashbin = $this->language->lang('TRASHBIN');
-					// See line 578, mcp_main.php
-					$this->phpbb_log->add('mod', $user_id, $this->user->ip, 'LOG_MOVE', false,
-						array(
+				// Logging
+				$trashbin = $this->language->lang('TRASHBIN');
+				$this->phpbb_log->add('mod', $this->user->data['user_id'], $this->user->ip, 'LOG_MOVE', false,
+					array(
 						'forum_id' => $target,
 						'topic_id' => $this->tid,
 						$forum_name,
 						$trashbin,
 						$this->fid,
 						$target,
-						));
+					));
 
-					// Redirection of browser to the trashbin
-					$params = "f=$target&amp;t=$this->tid";
-					$url = append_sid("{$this->root_path}viewtopic.$this->phpEx", $params);
-					redirect($url);
-				}
+				// Redirect to trashbin forum
+				$params = "f=$target&amp;t=$this->tid";
+				$url = append_sid("{$this->root_path}viewtopic.$this->phpEx", $params);
+				redirect($url);
 			}
 		}
-	}	//move_topic
-
-
+	}
 }
