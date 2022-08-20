d5191c38a60104c4347714c84b28d80d5351d9f7
Mario Skouat <forum@skouat.org>
2022-08-20 18:18:11 +0200

    Cleanup migration files

--- migrations/release_1.php
+++ migrations/release_1.php
@@ -1,11 +1,11 @@
 <?php
 /**
-*
-* @package phpBB Extension - LMDI Trashbin
-* @copyright (c) 2016-2019 Pierre Duhem - LMDI
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package phpBB Extension - LMDI Trashbin
+ * @copyright (c) 2016-2019 Pierre Duhem - LMDI
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 namespace lmdi\trashbin\migrations;
 
@@ -16,7 +16,7 @@ class release_1 extends \phpbb\db\migration\migration
 		return isset($this->config['lmdi_trashbin']);
 	}
 
-	static public function depends_on()
+	public static function depends_on()
 	{
 		return array('\phpbb\db\migration\data\v310\alpha2');
 	}
@@ -34,30 +34,14 @@ class release_1 extends \phpbb\db\migration\migration
 				'acp',
 				'ACP_TRASHBIN_TITLE',
 				array(
-					'module_basename'	=> '\lmdi\trashbin\acp\trashbin_module',
-					'auth'			=> 'ext_lmdi/trashbin && acl_a_board',
-					'modes'			=> array('settings'),
+					'module_basename' => '\lmdi\trashbin\acp\trashbin_module',
+					'auth'            => 'ext_lmdi/trashbin && acl_a_board',
+					'modes'           => array('settings'),
 				),
 			)),
 
 			// Configuration rows
 			array('config.add', array('lmdi_trashbin', 0)),
-
 		);
 	}
-
-	public function revert_data()
-	{
-		return array(
-			array('config.remove', array('lmdi_trashbin')),
-
-			array('module.remove', array(
-				'acp',
-				'ACP_CAT_DOT_MODS',
-				'ACP_TRASHBIN_TITLE',
-			)),
-
-		);
-	}
-
 }
--- migrations/release_2.php
+++ migrations/release_2.php
@@ -1,11 +1,11 @@
 <?php
 /**
-*
-* @package phpBB Extension - LMDI Trashbin extension
-* @copyright (c) 2016-2019 Pierre Duhem - LMDI
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package phpBB Extension - LMDI Trashbin extension
+ * @copyright (c) 2016-2019 Pierre Duhem - LMDI
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 namespace lmdi\trashbin\migrations;
 
@@ -19,7 +19,7 @@ class release_2 extends \phpbb\db\migration\migration
 	}
 
 
-	static public function depends_on()
+	public static function depends_on()
 	{
 		return array('\lmdi\trashbin\migrations\release_1');
 	}
@@ -28,9 +28,6 @@ class release_2 extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		$this->lmdi_trashbin = (int) $this->config['lmdi_trashbin'];
-		return array(
-			array('config.add', array('lmdi_trashbin2', 0)),
-		);
 	}
 
 
@@ -38,7 +35,6 @@ class release_2 extends \phpbb\db\migration\migration
 	{
 		return array(
 			array('custom', array(array(&$this, 'reset_pruning_state'))),
-			array('config.remove', array('lmdi_trashbin2')),
 		);
 	}
 
@@ -50,5 +46,4 @@ class release_2 extends \phpbb\db\migration\migration
 			WHERE forum_id=' . $this->lmdi_trashbin;
 		$this->db->sql_query($sql);
 	}
-
 }
