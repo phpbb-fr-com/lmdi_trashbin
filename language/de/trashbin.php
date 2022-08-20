daec869146f264f0c3c23a4aae2bcea9a77df44a
Mario Skouat <forum@skouat.org>
2022-08-20 16:59:54 +0200

    Use phpBB lang keys

--- language/de/trashbin.php
+++ language/de/trashbin.php
@@ -1,12 +1,13 @@
 <?php
 /**
-* trashbin.php
-* @package phpBB Extension - LMDI Trashbin
-* @copyright (c) 2015-2019 LMDI - Pierre Duhem
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* Deutsche übersetzung: Frank Ingermann
-*
-*/
+ * trashbin.php
+ *
+ * @package phpBB Extension - LMDI Trashbin
+ * @copyright (c) 2015-2019 LMDI - Pierre Duhem
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * Deutsche übersetzung: Frank Ingermann
+ *
+ */
 
 if (!defined('IN_PHPBB'))
 {
@@ -15,25 +16,17 @@ if (!defined('IN_PHPBB'))
 
 if (empty($lang) || !is_array($lang))
 {
-	$lang = array();
+	$lang = [];
 }
 
-$lang = array_merge($lang, array(
-	'TRASHBIN'		=> 'Forum-Papierkorb',
-	'TRASHBIN_TEXT'	=> 'Das Thema wurde vom %s ins Papierkorb geworfen (Quelle: %s).',
+$lang = array_merge($lang, [
+	'TRASHBIN_LINK'                      => 'In den Papierkorb werfen',
+	'TRASHBIN_TEXT'                      => 'Das Thema wurde vom %s ins Papierkorb geworfen (Quelle: %s).',
 
-// ACP
-	'ACP_TRASHBIN_TITLE'	=> 'Forum-Papierkorb',
-	'ACP_TRASHBIN_SETTINGS'	=> 'Einstellungen',
-	'ACP_TRASHBIN_ALLOW_FEATURE'		=> 'Zielforum auswählen',
-	'ACP_TRASHBIN_ALLOW_FEATURE_EXPLAIN'	=> 'Du kannst hier das Forum auswählen, das Ziel der Themen-Verlagerung sein wird.',
-
-	'ACP_TRASHBIN_FORUM_PRUNE_SETTINGS'		=> 'Einstellungen zum Löschen des Papierkorbs',
-	'ACP_TRASHBIN_FORUM_AUTO_PRUNE'			=> 'Automatisches Löschen aktivieren',
-	'ACP_TRASHBIN_FORUM_AUTO_PRUNE_EXPLAIN'	=> 'Löscht Themen des Papierkorbs automatisch, wenn sie den folgenden Kriterien entsprechen.',
-	'ACP_TRASHBIN_AUTO_PRUNE_DAYS'			=> 'Seit dem letzten Beitrag vergangene Tage',
-	'ACP_TRASHBIN_AUTO_PRUNE_DAYS_EXPLAIN'	=> 'Die Anzahl der Tage seit dem letzten Beitrag, nach denen das Thema gelöscht wird.',
-	'ACP_TRASHBIN_AUTO_PRUNE_FREQ'			=> 'Prüfungsintervall für automatisches Löschen',
-	'ACP_TRASHBIN_AUTO_PRUNE_FREQ_EXPLAIN'	=> 'Das Intervall, in dem nach automatisch zu löschenden Themen gesucht wird.',
-
-));
+	// ACP
+	'ACP_TRASHBIN_TITLE'                 => 'Forum-Papierkorb',
+	'ACP_TRASHBIN_SETTINGS'              => 'Einstellungen',
+	'ACP_TRASHBIN_ALLOW_FEATURE'         => 'Zielforum auswählen',
+	'ACP_TRASHBIN_ALLOW_FEATURE_EXPLAIN' => 'Du kannst hier das Forum auswählen, das Ziel der Themen-Verlagerung sein wird.',
+	'ACP_TRASHBIN_FORUM_PRUNE_SETTINGS'  => 'Einstellungen zum Löschen des Papierkorbs',
+]);
--- language/fr/trashbin.php
+++ language/fr/trashbin.php
@@ -1,11 +1,12 @@
 <?php
 /**
-* trashbin.php
-* @package phpBB Extension - LMDI Trashbin
-* @copyright (c) 2016-2019 LMDI - Pierre Duhem
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ * trashbin.php
+ *
+ * @package phpBB Extension - LMDI Trashbin
+ * @copyright (c) 2016-2019 LMDI - Pierre Duhem
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 if (!defined('IN_PHPBB'))
 {
@@ -14,24 +15,17 @@ if (!defined('IN_PHPBB'))
 
 if (empty($lang) || !is_array($lang))
 {
-	$lang = array();
+	$lang = [];
 }
 
-$lang = array_merge($lang, array(
-	'TRASHBIN'			=> 'Corbeille du forum',
-	'TRASHBIN_TEXT'		=> 'Sujet mis à la corbeille par %s (forum d’origine : %s).',
+$lang = array_merge($lang, [
+	'TRASHBIN_LINK'                         => 'Mettre à la corbeille',
+	'TRASHBIN_TEXT'                         => 'Sujet mis à la corbeille par %s (forum d’origine : %s).',
 
-// ACP
-	'ACP_TRASHBIN_TITLE'	=> 'Corbeille du forum',
-	'ACP_TRASHBIN_SETTINGS'	=> 'Configuration de l’extension',
-	'ACP_TRASHBIN_ALLOW_FEATURE'		=> 'Sélection du forum de destination',
-	'ACP_TRASHBIN_ALLOW_FEATURE_EXPLAIN'	=> 'Vous pouvez sélectionner ci-contre le forum qui sera la destination des sujets mis à la corbeille.',
-	'ACP_TRASHBIN_FORUM_PRUNE_SETTINGS'	=> 'Paramètres de délestage de la corbeille',
-	'ACP_TRASHBIN_FORUM_AUTO_PRUNE'		=> 'Activer l’auto-délestage',
-	'ACP_TRASHBIN_FORUM_AUTO_PRUNE_EXPLAIN'	=> 'Déleste le forum des sujets, réglez les paramètres de fréquence/ancienneté ci-dessous.',
-	'ACP_TRASHBIN_AUTO_PRUNE_DAYS'		=> 'Ancienneté des messages délestés automatiquement',
-	'ACP_TRASHBIN_AUTO_PRUNE_DAYS_EXPLAIN'	=> 'Nombre de jours depuis le dernier message avant suppression du sujet.',
-	'ACP_TRASHBIN_AUTO_PRUNE_FREQ'		=> 'Fréquence du délestage automatique',
-	'ACP_TRASHBIN_AUTO_PRUNE_FREQ_EXPLAIN'	=> 'Durée en jours entre les événements de délestage.',
-
-));
+	// ACP
+	'ACP_TRASHBIN_TITLE'                    => 'Corbeille du forum',
+	'ACP_TRASHBIN_SETTINGS'                 => 'Paramètres généraux',
+	'ACP_TRASHBIN_ALLOW_FEATURE'            => 'Sélectionner le forum cible',
+	'ACP_TRASHBIN_ALLOW_FEATURE_EXPLAIN'    => 'Sélectionnez le forum qui fera office de corbeille.',
+	'ACP_TRASHBIN_FORUM_PRUNE_SETTINGS'     => 'Paramètres de délestage de la corbeille',
+]);
